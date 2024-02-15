06e2bcd05d2f00bd08e30459aa1fd1cfd402e8d8
Abraham Agusti <abraham.agusti@gmail.com>
2024-02-15 21:48:55 +0100

    Starting HTML, CSS and Words

---
+++ src/words.js
@@ -0,0 +1,42 @@
+const words = [
+    {
+        word:"Peach",
+        hint:"Furry fruit",
+    },
+    {
+        word:"Microphone",
+        hint:"Device for singing",
+    },
+    {
+        word:"Book",
+        hint:"To read",
+    },
+    {
+        word:"Wisdom",
+        hint:"Wizards are",
+    },
+    {
+        word:"Switch",
+        hint:"Turn the light on",
+    },
+    {
+        word:"Ship",
+        hint:"For sailing",
+    },
+    {
+        word:"Slippers",
+        hint:"Shoes for home",
+    },
+    {
+        word:"Donkey",
+        hint:"Animal for carry",
+    },
+    {
+        word:"Pasta",
+        hint:"Italian food",
+    },
+    {
+        word:"Water",
+        hint:"If you are thirsty",
+    },
+]
